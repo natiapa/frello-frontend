5431e95cac119ded90388b06d41893fa363e5b04
Yana <yana102hanan@gmail.com>
2024-08-19 11:36:20 +0300

    Add home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,26 @@
-export function HomePage() {
-    return (
-        <section>
-            <h1>Home sweet Home</h1>
-        </section >
-    )
-}
+import HomeImg from "../assets/imgs/home-img.webp";
+import { Link } from "react-router-dom";
 
+export function HomePage() {
+  return (
+    <section>
+      <section className="home-page">
+        <section className="guest-home-page">
+          <div className="text-container">
+            <h1>Trello brings all your tasks, teammates, and tools together</h1>
+            <p>Keep everything in the same place-even if your team isn’t.</p>
+
+            <div className="signup-btn-container">
+              <Link to={"/auth/signup"}>
+                <button>Sign up - it's free!</button>
+              </Link>
+            </div>
+          </div>
+          <div className="homepage-img-container">
+            <img src={HomeImg} alt="" />
+          </div>
+        </section>
+      </section>
+    </section>
+  );
+}
