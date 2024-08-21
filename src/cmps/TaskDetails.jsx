7fde24de737c12e68dc9d2ca7173b2064e405c90
Yana <yana102hanan@gmail.com>
2024-08-21 19:30:38 +0300

    Add close button svg

--- src/cmps/TaskDetails.jsx
+++ src/cmps/TaskDetails.jsx
@@ -7,12 +7,30 @@ export function TaskDetails({ boardId, task }) {
   function onCloseForm() {
     navigate(`/board/${boardId}`);
   }
+
   return (
     <>
       <div className="overlay" onClick={onCloseForm}></div>
       <div className="task-details">
         <form onSubmit={onCloseForm}>
-          <button>X</button>
+          <button>
+            <span>
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m4 4 8 8m-8 0 8-8"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeWidth="1.5"
+                ></path>
+              </svg>
+            </span>
+          </button>
           <h1>{task.title}</h1>
           <ul>
             <h3>Labels:</h3>
