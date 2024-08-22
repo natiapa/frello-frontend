b70e667836d3b2f95abf1c4cf44813026a0866cd
Yana <yana102hanan@gmail.com>
2024-08-22 15:19:25 +0300

    fix lines

--- src/pages/BoardDetails.jsx
+++ src/pages/BoardDetails.jsx
@@ -1,66 +1,61 @@
-import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { Link ,Outlet } from "react-router-dom"
+import { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link, Outlet } from "react-router-dom";
 
-import { showSuccessMsg, showErrorMsg } from "../services/event-bus.service"
-import { loadBoard, addBoardMsg } from "../store/actions/board.actions"
+import { showSuccessMsg, showErrorMsg } from "../services/event-bus.service";
+import { loadBoard, addBoardMsg } from "../store/actions/board.actions";
 
-import { GroupList } from "../cmps/GroupList"
-import { SideBar } from "../cmps/Sidebar"
-import { BoardHeader } from "../cmps/BoardHeader"
+import { GroupList } from "../cmps/GroupList";
+import { SideBar } from "../cmps/Sidebar";
+import { BoardHeader } from "../cmps/BoardHeader";
 // import { Outlet } from "react-router-dom"
-import { BoardSideBar } from "../cmps/BoardSideBar"
-import { AppHeader } from "../cmps/AppHeader"        
+import { BoardSideBar } from "../cmps/BoardSideBar";
+import { AppHeader } from "../cmps/AppHeader";
 
-import { FastAverageColor } from "fast-average-color"
+import { FastAverageColor } from "fast-average-color";
 
-import {
-  updateBoard,
-} from "../store/actions/board.actions"
-
-
+import { updateBoard } from "../store/actions/board.actions";
 
 export function BoardDetails() {
-  const { boardId } = useParams()
-  const [headerBgColor, setHeaderBgColor] = useState()
+  const { boardId } = useParams();
+  const [bgColor, setbgColor] = useState();
 
-  const board = useSelector((storeState) => storeState.boardModule.board)
+  const board = useSelector((storeState) => storeState.boardModule.board);
 
   useEffect(() => {
     loadBoard(boardId);
   }, [boardId]);
 
   useEffect(() => {
-    async function calculateBgColor() {
-      if (board?.style?.backgroundImage) {
-        const fac = new FastAverageColor();
-        try {
-          const color = await fac.getColorAsync(board.style.backgroundImage)
-          setHeaderBgColor(color.hex);
-       
-        } catch (error) {
-          console.error("Failed to calculate background color:", error)
-        }
-      }
-    }
     calculateBgColor();
-  }, [board?.style?.backgroundImage])
+  }, [board?.style?.backgroundImage]);
 
-  async function onUpdateBoard(newBoard) {
-    try {
-      await updateBoard(newBoard)
-      loadBoard(boardId)//***check if there is other way.***
-      // showSuccessMsg("Board U");
-    } catch (err) {
-      showErrorMsg("Cannot update board")
+  async function calculateBgColor() {
+    if (board?.style?.backgroundImage) {
+      const fac = new FastAverageColor();
+      try {
+        const color = await fac.getColorAsync(board.style.backgroundImage);
+        setbgColor(color.hex);
+      } catch (error) {
+        console.error("Failed to calculate background color:", error);
+      }
     }
   }
 
+  async function onUpdateBoard(newBoard) {
+    try {
+      await updateBoard(newBoard);
+      loadBoard(boardId); //***check if there is other way.***
+      // showSuccessMsg("Board U");
+    } catch (err) {
+      showErrorMsg("Cannot update board");
+    }
+  }
 
   async function onAddBoardMsg(boardId) {
     try {
-      await addBoardMsg(boardId, "bla bla " + parseInt(Math.random() * 10))
+      await addBoardMsg(boardId, "bla bla " + parseInt(Math.random() * 10));
       showSuccessMsg(`Board msg added`);
     } catch (err) {
       showErrorMsg("Cannot add board msg");
@@ -80,10 +75,10 @@ export function BoardDetails() {
           backgroundImage: `url(${board?.style?.backgroundImage})`,
         }}
       >
-        <AppHeader bgColor={headerBgColor} />
-        <BoardHeader bgColor={headerBgColor} />
+        <AppHeader bgColor={bgColor} />
+        <BoardHeader bgColor={bgColor} />
 
-        {board && <BoardSideBar board={board} bgColor={headerBgColor} />}
+        {board && <BoardSideBar board={board} bgColor={bgColor} />}
         {board && <GroupList groups={board.groups} />}
         {/* {taskId && <TaskDetails board={board} group={group} task={task} onUpdateBoard={onUpdateBoard} />} */}
 
@@ -94,7 +89,7 @@ export function BoardDetails() {
                 Add board msg
             </button> */}
 
-          <Outlet/>
+        <Outlet />
       </section>
     </>
   );
