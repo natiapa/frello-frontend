4405c7154ed405b31d00c00e8224e0622fc3cbee
natiapa <natiapanosian@gmail.com>
2024-08-25 20:26:13 +0300

    Add new item to checklist

--- src/cmps/TaskChecklist.jsx
+++ src/cmps/TaskChecklist.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
+import { boardService } from "../services/board/board.service.local"
 
 export function TaskChecklist({ checklists, onUpdatedTask }) {
     const [checklistsState, setChecklists] = useState(checklists)
+    const [isAddingItem, setIsAddingItem] = useState(null)
+    const [newItem, setNewItem] = useState('')
 
     function handleChecklistItem({ target }, checklistId, itemId) {
-
         const isChecked = target.checked
 
         const updatedChecklists = checklistsState.map(checklist =>
@@ -14,10 +16,41 @@ export function TaskChecklist({ checklists, onUpdatedTask }) {
                     item.id === itemId ? { ...item, isChecked } : item
                 ),
             } : checklist
-        );
+        )
 
         setChecklists(updatedChecklists)
         onUpdatedTask('checklists', updatedChecklists)
+    }
+
+    function onAddItem(checklistId) {
+        setIsAddingItem(checklistId)
+    }
+    function onCloseItem(checklistId) {
+        setIsAddingItem(null)
+        setNewItem('') 
+    }
+
+    function onSaveItem() {
+        const updatedChecklists = checklistsState.map(checklist => {
+            if (checklist.id === isAddingItem) {
+                return {
+                    ...checklist,
+                    items: [
+                        ...checklist.items,
+                        {
+                            ...boardService.getEmptyItem(),
+                            text: newItem,
+                        }
+                    ]
+                }
+            }
+            return checklist
+        })
+
+        setChecklists(updatedChecklists)
+        onUpdatedTask('checklists', updatedChecklists)
+        setIsAddingItem(null)
+        setNewItem('') 
     }
 
     return (
@@ -35,13 +68,28 @@ export function TaskChecklist({ checklists, onUpdatedTask }) {
                                 />
                                 {item.text}
                             </li>
-                        )
-
-                        )}
+                        ))}
                     </ul>
+                    <div>
+                        {isAddingItem === checklist.id ? (
+                            <div>
+                                <input
+                                    type="text"
+                                    placeholder="Enter new item"
+                                    value={newItem}
+                                    onChange={(ev) => setNewItem(ev.target.value)}
+                                />
+                                <button onClick={onSaveItem}>Save</button>
+                                <button onClick={onCloseItem}>cancel</button>
+                            </div>
+                        ) : (
+                            <button onClick={() => onAddItem(checklist.id)}>Add an item</button>
+                        )}
+                    </div>
                 </div>
-            ))
-            }
+            ))}
         </div>
     )
 }
+
+
--- src/cmps/TaskDetails.jsx
+++ src/cmps/TaskDetails.jsx
@@ -8,10 +8,10 @@ import { TaskChecklist } from './TaskChecklist'
 
 import { boardService } from '../services/board/board.service.local'
 import { updateBoard } from '../store/actions/board.actions'
-import { CgList } from 'react-icons/cg'
-import { BiCheckbox } from 'react-icons/bi'
-import { IoMdCheckboxOutline } from 'react-icons/io'
-import { TaskDetailsActions } from './TaskDetailsActions'
+// import { CgList } from 'react-icons/cg'
+// import { BiCheckbox } from 'react-icons/bi'
+// import { IoMdCheckboxOutline } from 'react-icons/io'
+// import { TaskDetailsActions } from './TaskDetailsActions'
 
 export function TaskDetails() {
     const [currElementToEdit, setCurrElementToEdit] = useState('')
@@ -22,9 +22,9 @@ export function TaskDetails() {
 
     const { boardId, groupId, taskId } = params
 
-    const board = useSelector(storeState => storeState.boardModule.board)
-    const group = board?.groups?.find(group => group.id === groupId)
-    const task = group?.tasks?.find(task => task.id === taskId)
+    const board = useSelector((storeState) => storeState.boardModule.board)
+    const group = board?.groups?.find((group) => group.id === groupId)
+    const task = group?.tasks?.find((task) => task.id === taskId)
 
     useEffect(() => {
         if (dialogRef.current) {
@@ -53,7 +53,6 @@ export function TaskDetails() {
 
     function onEdit(ev) {
         const dataName = ev.currentTarget.getAttribute('data-name')
-        // console.log(dataName)
         setCurrElementToEdit(dataName)
     }
 
@@ -72,7 +71,8 @@ export function TaskDetails() {
         navigate(`/board/${boardId}`)
     }
 
-    if (!task) return
+    if (!task) return null
+
     return (
         <section className="task-details">
             <dialog ref={dialogRef} method="dialog" onClick={handleDialogClick}>
@@ -82,7 +82,12 @@ export function TaskDetails() {
                             {task?.title || 'Untitled Task'}
                         </header>
                     ) : (
-                        <EditTask task={task} onUpdatedTask={onUpdatedTask} currElementToEdit={currElementToEdit} setCurrElementToEdit={setCurrElementToEdit} />
+                        <EditTask
+                            task={task}
+                            onUpdatedTask={onUpdatedTask}
+                            currElementToEdit={currElementToEdit}
+                            setCurrElementToEdit={setCurrElementToEdit}
+                        />
                     )}
                     <button className="close-btn" onClick={onCloseDialog}>
                         x
@@ -95,20 +100,143 @@ export function TaskDetails() {
                             {task?.description || 'Add a more detailed description...'}
                         </p>
                     ) : (
-                        <EditTask task={task} onUpdatedTask={onUpdatedTask} currElementToEdit={currElementToEdit} setCurrElementToEdit={setCurrElementToEdit} />
+                        <EditTask
+                            task={task}
+                            onUpdatedTask={onUpdatedTask}
+                            currElementToEdit={currElementToEdit}
+                            setCurrElementToEdit={setCurrElementToEdit}
+                        />
                     )}
 
                     <button className="delete-btn" onClick={deleteTask}>
                         Delete task
                     </button>
 
-                    {task?.checklists && task.checklists.length > 0 && <TaskChecklist checklists={task.checklists} onUpdatedTask={onUpdatedTask} />}
+                    {task?.checklists && task.checklists.length > 0 && (
+                        <TaskChecklist checklists={task.checklists} onUpdatedTask={onUpdatedTask} />
+                    )}
                 </form>
                 {/* <TaskDetailsActions /> */}
             </dialog>
         </section>
     )
 }
+
+
+
+
+
+// import { useEffect, useRef, useState } from 'react'
+// import { useNavigate, useParams } from 'react-router'
+// import { useSelector } from 'react-redux'
+
+// import { EditTask } from './EditTask'
+// import { LabelList } from './LabelList'
+// import { TaskChecklist } from './TaskChecklist'
+
+// import { boardService } from '../services/board/board.service.local'
+// import { updateBoard } from '../store/actions/board.actions'
+// import { CgList } from 'react-icons/cg'
+// import { BiCheckbox } from 'react-icons/bi'
+// import { IoMdCheckboxOutline } from 'react-icons/io'
+// import { TaskDetailsActions } from './TaskDetailsActions'
+
+// export function TaskDetails() {
+//     const [currElementToEdit, setCurrElementToEdit] = useState('')
+
+//     const dialogRef = useRef(null)
+//     const params = useParams()
+//     const navigate = useNavigate()
+
+//     const { boardId, groupId, taskId } = params
+
+//     const board = useSelector(storeState => storeState.boardModule.board)
+//     const group = board?.groups?.find(group => group.id === groupId)
+//     const task = group?.tasks?.find(task => task.id === taskId)
+
+//     useEffect(() => {
+//         if (dialogRef.current) {
+//             dialogRef.current.showModal()
+//         }
+//     }, [params])
+
+//     async function onUpdatedTask(name, value) {
+//         try {
+//             const updatedBoard = boardService.updateBoard(board, groupId, taskId, {
+//                 key: name,
+//                 value: value,
+//             })
+//             await updateBoard(updatedBoard)
+//         } catch (error) {
+//             console.error('Failed to update the board:', error)
+//         }
+//     }
+
+//     function onCloseDialog() {
+//         navigate(`/board/${boardId}`)
+//         if (dialogRef.current) {
+//             dialogRef.current.close()
+//         }
+//     }
+
+//     function onEdit(ev) {
+//         const dataName = ev.currentTarget.getAttribute('data-name')
+//         // console.log(dataName)
+//         setCurrElementToEdit(dataName)
+//     }
+
+//     function handleDialogClick(ev) {
+//         if (ev.target === dialogRef.current) {
+//             onCloseDialog()
+//         }
+//     }
+
+//     function deleteTask(ev) {
+//         ev.preventDefault()
+//         boardService.updateBoard(board, groupId, taskId, {
+//             key: 'deleteTask',
+//             value: null,
+//         })
+//         navigate(`/board/${boardId}`)
+//     }
+
+//     if (!task) return
+//     return (
+//         <section className="task-details">
+//             <dialog ref={dialogRef} method="dialog" onClick={handleDialogClick}>
+//                 <form>
+//                     {!currElementToEdit || currElementToEdit !== 'title' ? (
+//                         <header data-name="title" onClick={onEdit}>
+//                             {task?.title || 'Untitled Task'}
+//                         </header>
+//                     ) : (
+//                         <EditTask task={task} onUpdatedTask={onUpdatedTask} currElementToEdit={currElementToEdit} setCurrElementToEdit={setCurrElementToEdit} />
+//                     )}
+//                     <button className="close-btn" onClick={onCloseDialog}>
+//                         x
+//                     </button>
+
+//                     <LabelList labels={task.labels} />
+
+//                     {!currElementToEdit || currElementToEdit !== 'description' ? (
+//                         <p className="editable-description" data-name="description" onClick={onEdit}>
+//                             {task?.description || 'Add a more detailed description...'}
+//                         </p>
+//                     ) : (
+//                         <EditTask task={task} onUpdatedTask={onUpdatedTask} currElementToEdit={currElementToEdit} setCurrElementToEdit={setCurrElementToEdit} />
+//                     )}
+
+//                     <button className="delete-btn" onClick={deleteTask}>
+//                         Delete task
+//                     </button>
+
+//                     {task?.checklists && task.checklists.length > 0 && <TaskChecklist checklists={task.checklists} onUpdatedTask={onUpdatedTask} />}
+//                 </form>
+//                 {/* <TaskDetailsActions /> */}
+//             </dialog>
+//         </section>
+//     )
+// }
 // return (
 //   <>
 //     <dialog
