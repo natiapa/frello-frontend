678a976c0b846792977ed0e74c65888e2c6285c3
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2024-08-27 23:36:12 +0300

    Add form to add group

--- src/cmps/GroupList.jsx
+++ src/cmps/GroupList.jsx
@@ -3,6 +3,7 @@ import { GroupPreview } from './GroupPreview'
 import { boardService } from '../services/board'
 import { useSelector } from 'react-redux'
 import { updateBoard } from '../store/actions/board.actions'
+import { IoMdClose } from 'react-icons/io'
 
 export function GroupList({ groups }) {
     const [isNewGroup, setIsNewGroup] = useState(false)
@@ -12,14 +13,27 @@ export function GroupList({ groups }) {
     useEffect(() => {
         console.log('groups:', groups)
     }, [groups?.length])
-    
-    function onAddGroup() {
-        setNewGroup(boardService.getEmptyGroup())
-        setIsNewGroup(true)
+
+    function handelChange(ev) {
+        const { name, value } = ev.target
+        setNewGroup({ ...newGroup, [name]: value })
+    }
+
+    function onSaveGroup(ev) {
+        ev.preventDefault()
         const updatedBoard = { ...currBoard }
         updatedBoard.groups.push(newGroup)
         updateBoard(updatedBoard)
         setIsNewGroup(false)
+    }
+
+    function onCloseForm() {
+        setIsNewGroup(false)
+    }
+
+    function onAddGroup() {
+        setIsNewGroup(true)
+        setNewGroup(boardService.getEmptyGroup())
     }
 
     if (!groups) return <div>Loading...</div>
@@ -28,10 +42,21 @@ export function GroupList({ groups }) {
             {groups.map(group => (
                 <GroupPreview group={group} key={group.id} />
             ))}
-            <div className="add-group-preview">
-                {isNewGroup && <GroupPreview group={newGroup} key={newGroup.id}/>}
-                <button className='add-group-btn' onClick={onAddGroup}><span className='header-btn'>+ Add another list</span></button>
-            </div>
+            {/* <div className="add-group-preview"> */}
+            {isNewGroup && (
+                <form onSubmit={onSaveGroup}>
+                    <textarea onChange={handelChange} rows="1" name="title" placeholder="Enter list name…"></textarea>
+                    <button className="save-group-btn">Add list</button>
+                    <button className="close-btn" onClick={onCloseForm}>
+                        <span><IoMdClose /></span>
+                    </button>
+                </form>
+            )}
+            {/* {isNewGroup && <GroupPreview group={newGroup} key={newGroup.id}/>} */}
+            <button className="add-group-btn" onClick={onAddGroup} hidden={isNewGroup}>
+                <span className="header-btn">+ Add another list</span>
+            </button>
+            {/* </div> */}
         </div>
     )
 }
