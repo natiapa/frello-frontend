8f657f3f3dfcd60760b9bc6b92ce6bc3578cfd5d
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2024-08-29 20:46:56 +0300

    Checklists bugs

--- src/cmps/TaskChecklist.jsx
+++ src/cmps/TaskChecklist.jsx
@@ -1,106 +1,109 @@
-import { useState } from "react";
-import { useNavigate } from "react-router";
-import { boardService } from "../services/board";
-import { Checklist } from "./Checklist";
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+import { boardService } from '../services/board'
+import { Checklist } from './Checklist'
 
-export function TaskChecklist({
-  checklists,
-  onUpdated,
-  task,
-  groupId,
-  boardId,
-}) {
-  const [checklistsState, setChecklistsState] = useState(checklists || [])
-  const [isAddingItem, setIsAddingItem] = useState(null)
-  const [hideCheckedItems, setHideCheckedItems] = useState(false)
-  const [newItem, setNewItem] = useState("")
-  const navigate = useNavigate()
+export function TaskChecklist({ checklists, onUpdated, task, groupId, boardId }) {
+    const [checklistsState, setChecklistsState] = useState(checklists || [])
+    const [isAddingItem, setIsAddingItem] = useState(null)
+    const [hideCheckedItems, setHideCheckedItems] = useState(false)
+    const [newItem, setNewItem] = useState('')
+    const navigate = useNavigate()
+    console.log('checklistsState:', checklistsState)
 
-  function handleChecklistItem({ target }, checklistId, itemId, editedText) {
-    
-    const isChecked = target.checked;
-  
-    const updatedChecklists = checklistsState.map((checklist) =>
-      checklist.id === checklistId
-        ? {
-            ...checklist,
-            items: checklist.items.map((item) =>
-              item.id === itemId
-                ? { ...item, isChecked, text: editedText?.trim() ? editedText : item.text }
-                : item
-            ),
-          }
-        : checklist
+    function handleChecklistItem({ target }, checklist, itemId, editedText) {
+        console.log('checklist:', checklist)
+        //   console.log('1:',checklistId,'2:', itemId, '3:',editedText);
+
+        //   const isChecked = target.checked;
+        // console.log('isChecked:', isChecked)
+        const selectedChecklist = checklistsState.filter(currChecklist => currChecklist.id === checklist.id)
+        console.log('selectedChecklist:', selectedChecklist)
+        const selectedItem = selectedChecklist[0].items.find(currItem => currItem.id === itemId)
+        const updateIsChecked = !selectedItem.isChecked
+        const itemToUpdate = { ...selectedItem, isChecked: updateIsChecked }
+        console.log('itemToUpdate:', itemToUpdate)
+        onUpdated('checklist',itemToUpdate)
+        setChecklistsState(selectedChecklist)
+        // currChecklist.id === checklist.id
+        //   ? {
+        //       ...currChecklist,
+        //       items: checklist.items.map((item) =>
+        //         item.id === itemId
+        //           ? { ...item, isChecked, text: editedText?.trim() ? editedText : item.text }
+        //           : item
+        //       ),
+        //     }
+        //   : checklist
+
+        // onUpdated('checklists', updatedChecklists)
+        // console.log('updatedChecklists:', updatedChecklists)
+    }
+
+    function handleHideItems() {
+        setHideCheckedItems(!hideCheckedItems)
+    }
+
+    function onAddItem(checklistId) {
+        setIsAddingItem(checklistId)
+    }
+
+    function onCloseItem() {
+        setIsAddingItem(null)
+        setNewItem('')
+    }
+
+    function onSaveItem() {
+        const updatedChecklists = checklistsState.map(checklist => {
+            if (checklist.id === isAddingItem) {
+                return {
+                    ...checklist,
+                    items: [
+                        ...checklist.items,
+                        {
+                            ...boardService.getEmptyItem(),
+                            text: newItem,
+                        },
+                    ],
+                }
+            }
+            console.log('checklist:', checklist)
+            return checklist
+        })
+
+        setChecklistsState(updatedChecklists)
+        onUpdated('checklists', updatedChecklists)
+        setIsAddingItem(null)
+        setNewItem('')
+    }
+
+    function onRemoveChecklist(checklistId) {
+        const updatedChecklists = task.checklists.filter(checklist => checklist.id !== checklistId)
+        setChecklistsState(updatedChecklists)
+        onUpdated('checklists', updatedChecklists)
+        navigate(`/board/${boardId}/${groupId}/${task.id}`)
+    }
+
+    return (
+        <div className="task-checklists">
+            {checklistsState.map(checklist => (
+                <Checklist
+                    key={checklist.id}
+                    checklist={checklist}
+                    handleHideItems={handleHideItems}
+                    handleChecklistItem={handleChecklistItem}
+                    onRemoveChecklist={onRemoveChecklist}
+                    onAddItem={onAddItem}
+                    isAddingItem={isAddingItem}
+                    hideCheckedItems={hideCheckedItems}
+                    newItem={newItem}
+                    setNewItem={setNewItem}
+                    onSaveItem={onSaveItem}
+                    onCloseItem={onCloseItem}
+                    onUpdated={onUpdated}
+                    checklists={checklists}
+                />
+            ))}
+        </div>
     )
-  
-    setChecklistsState(updatedChecklists);
-    onUpdated("checklists", updatedChecklists);
-  }
-  
-  function handleHideItems() {
-    setHideCheckedItems(!hideCheckedItems)
-  }
-
-  function onAddItem(checklistId) {
-    setIsAddingItem(checklistId);
-  }
-
-  function onCloseItem() {
-    setIsAddingItem(null);
-    setNewItem("");
-  }
-
-  function onSaveItem() {
-    const updatedChecklists = checklistsState.map((checklist) => {
-      if (checklist.id === isAddingItem) {
-        return {
-          ...checklist,
-          items: [
-            ...checklist.items,
-            {
-              ...boardService.getEmptyItem(),
-              text: newItem,
-            },
-          ],
-        }
-      }
-      return checklist;
-    })
-
-    setChecklistsState(updatedChecklists);
-    onUpdated("checklists", updatedChecklists);
-    setIsAddingItem(null);
-    setNewItem("");
-  }
-
-  function onRemoveChecklist(checklistId) {
-    const updatedChecklists = task.checklists.filter(
-      (checklist) => checklist.id !== checklistId
-    );
-    setChecklistsState(updatedChecklists);
-    onUpdated("checklists", updatedChecklists);
-    navigate(`/board/${boardId}/${groupId}/${task.id}`);
-  }
-
-  return (
-    <div className="task-checklists">
-      {checklistsState.map((checklist) => (
-        <Checklist
-          key={checklist.id}
-          checklist={checklist}
-          handleHideItems={handleHideItems}
-          handleChecklistItem={handleChecklistItem}
-          onRemoveChecklist={onRemoveChecklist}
-          onAddItem={onAddItem}
-          isAddingItem={isAddingItem}
-          hideCheckedItems={hideCheckedItems}
-          newItem={newItem}
-          setNewItem={setNewItem}
-          onSaveItem={onSaveItem}
-          onCloseItem={onCloseItem}
-    
-        />
-      ))}
-    </div>
-  )
 }
--- src/cmps/TaskDetails.jsx
+++ src/cmps/TaskDetails.jsx
@@ -1,146 +1,118 @@
-import { useEffect, useRef, useState } from "react";
-import { useNavigate, useParams } from "react-router";
-import { useSelector } from "react-redux";
-import { Edit } from "./Edit";
-import { LabelList } from "./LabelList";
-import { TaskChecklist } from "./TaskChecklist";
-import { updateBoard } from "../store/actions/board.actions";
-import { TaskDetailsActions } from "./TaskDetailsActions";
-import { MemberList } from "./MemberList";
-import { boardService } from "../services/board";
-import { DueDatePicker } from "./DueDatePicker";
+import { useEffect, useRef, useState } from 'react'
+import { useNavigate, useParams } from 'react-router'
+import { useSelector } from 'react-redux'
+import { Edit } from './Edit'
+import { LabelList } from './LabelList'
+import { TaskChecklist } from './TaskChecklist'
+import { updateBoard } from '../store/actions/board.actions'
+import { TaskDetailsActions } from './TaskDetailsActions'
+import { MemberList } from './MemberList'
+import { boardService } from '../services/board'
+import { DueDatePicker } from './DueDatePicker'
 
 export function TaskDetails() {
-  const dialogRef = useRef(null);
-  const params = useParams();
-  const navigate = useNavigate();
-  const { boardId, groupId, taskId } = params;
-  const board = useSelector((storeState) => storeState.boardModule.board);
-  const group = board?.groups?.find((group) => group.id === groupId);
-  const task = group?.tasks?.find((task) => task.id === taskId);
+    const dialogRef = useRef(null)
+    const params = useParams()
+    const navigate = useNavigate()
+    const { boardId, groupId, taskId } = params
+    const board = useSelector(storeState => storeState.boardModule.board)
+    const group = board?.groups?.find(group => group.id === groupId)
+    const task = group?.tasks?.find(task => task.id === taskId)
 
-  const [currElToEdit, setCurrElToEdit] = useState("");
-  const [selectedLabels, setSelectedLabels] = useState(task.labels);
+    const [currElToEdit, setCurrElToEdit] = useState('')
+    const [selectedLabels, setSelectedLabels] = useState(task.labels)
 
-  useEffect(() => {
-    if (dialogRef.current) {
-      dialogRef.current.showModal();
+    useEffect(() => {
+        if (dialogRef.current) {
+            dialogRef.current.showModal()
+        }
+    }, [params])
+
+    async function onUpdated(name, value) {
+        console.log('name:', name)
+        console.log('value:', value)
+        try {
+            const updatedBoard = boardService.updateBoard(board, groupId, taskId, {
+                key: name,
+                value: value,
+            })
+            await updateBoard(updatedBoard)
+        } catch (error) {
+            console.error('Failed to update the board:', error)
+        }
     }
-  }, [params]);
 
-  async function onUpdated(name, value) {
-    try {
-      const updatedBoard = boardService.updateBoard(board, groupId, taskId, {
-        key: name,
-        value: value,
-      });
-      await updateBoard(updatedBoard);
-    } catch (error) {
-      console.error("Failed to update the board:", error);
+    function onEdit(ev) {
+        const dataName = ev.currentTarget.getAttribute('data-name')
+        setCurrElToEdit(dataName)
     }
-  }
 
-  function onEdit(ev) {
-    const dataName = ev.currentTarget.getAttribute("data-name");
-    setCurrElToEdit(dataName);
-  }
-
-  function onCloseDialog() {
-    navigate(`/board/${boardId}`);
-    if (dialogRef.current) {
-      dialogRef.current.close();
+    function onCloseDialog() {
+        navigate(`/board/${boardId}`)
+        if (dialogRef.current) {
+            dialogRef.current.close()
+        }
     }
-  }
 
-  function handleDialogClick(ev) {
-    if (ev.target === dialogRef.current) {
-      onCloseDialog();
+    function handleDialogClick(ev) {
+        if (ev.target === dialogRef.current) {
+            onCloseDialog()
+        }
     }
-  }
 
-  async function deleteTask(ev) {
-    ev.preventDefault();
-    try {
-      const updatedBoard = boardService.updateBoard(board, groupId, taskId, {
-        key: "deleteTask",
-        value: null,
-      });
-      await updateBoard(updatedBoard);
-      navigate(`/board/${boardId}`);
-    } catch (error) {
-      console.error("Failed to delete task:", error);
+    async function deleteTask(ev) {
+        ev.preventDefault()
+        try {
+            const updatedBoard = boardService.updateBoard(board, groupId, taskId, {
+                key: 'deleteTask',
+                value: null,
+            })
+            await updateBoard(updatedBoard)
+            navigate(`/board/${boardId}`)
+        } catch (error) {
+            console.error('Failed to delete task:', error)
+        }
     }
-  }
 
-  if (!task) return;
+    if (!task) return
 
-  return (
-    <section className="task-details">
-      <dialog ref={dialogRef} method="dialog" onClick={handleDialogClick}>
-        <form>
-          {currElToEdit !== "title" ? (
-            <header data-name="title" onClick={onEdit}>
-              {task?.title || "Untitled Task"}
-            </header>
-          ) : (
-            <Edit
-              task={task}
-              onUpdated={onUpdated}
-              currElToEdit={currElToEdit}
-              setCurrElToEdit={setCurrElToEdit}
-            />
-          )}
-          <button className="close-btn" onClick={onCloseDialog}>
-            x
-          </button>
-          <ul className="member-list">
-            <p className="header-member-list">Members</p>
+    return (
+        <section className="task-details">
+            <dialog ref={dialogRef} method="dialog" onClick={handleDialogClick}>
+                <form>
+                    {currElToEdit !== 'title' ? (
+                        <header data-name="title" onClick={onEdit}>
+                            {task?.title || 'Untitled Task'}
+                        </header>
+                    ) : (
+                        <Edit task={task} onUpdated={onUpdated} currElToEdit={currElToEdit} setCurrElToEdit={setCurrElToEdit} />
+                    )}
+                    <button className="close-btn" onClick={onCloseDialog}>
+                        x
+                    </button>
+                    <ul className="member-list">
+                        <p className="header-member-list">Members</p>
 
-            {task.members && <MemberList members={task.members} />}
-          </ul>
-          <LabelList labels={selectedLabels} />
+                        {task.members && <MemberList members={task.members} />}
+                    </ul>
+                    <LabelList labels={selectedLabels} />
 
-          {currElToEdit !== "description" ? (
-            <p
-              className="editable-description"
-              data-name="description"
-              onClick={onEdit}
-            >
-              {task?.description || "Add a more detailed description..."}
-            </p>
-          ) : (
-            <Edit
-              task={task}
-              onUpdated={onUpdated}
-              currElToEdit={currElToEdit}
-              setCurrElToEdit={setCurrElToEdit}
-            />
-          )}
+                    {currElToEdit !== 'description' ? (
+                        <p className="editable-description" data-name="description" onClick={onEdit}>
+                            {task?.description || 'Add a more detailed description...'}
+                        </p>
+                    ) : (
+                        <Edit task={task} onUpdated={onUpdated} currElToEdit={currElToEdit} setCurrElToEdit={setCurrElToEdit} />
+                    )}
 
-          <button className="delete-btn" onClick={deleteTask}>
-            Delete task
-          </button>
+                    <button className="delete-btn" onClick={deleteTask}>
+                        Delete task
+                    </button>
 
-          {task?.checklists && task.checklists.length > 0 && (
-            <TaskChecklist
-              checklists={task.checklists}
-              onUpdated={onUpdated}
-              task={task}
-              groupId={groupId}
-              boardId={boardId}
-            />
-          )}
-        </form>
-        <TaskDetailsActions
-          boardId={board?.id}
-          groupId={group.id}
-          taskId={task.id}
-          task={task}
-          selectedLabels={selectedLabels}
-          setSelectedLabels={setSelectedLabels}
-          onUpdated={onUpdated}
-        />
-      </dialog>
-    </section>
-  );
+                    {task?.checklists && task.checklists.length > 0 && <TaskChecklist checklists={task.checklists} onUpdated={onUpdated} task={task} groupId={groupId} boardId={boardId} />}
+                </form>
+                <TaskDetailsActions boardId={board?.id} groupId={group.id} taskId={task.id} task={task} selectedLabels={selectedLabels} setSelectedLabels={setSelectedLabels} onUpdated={onUpdated} />
+            </dialog>
+        </section>
+    )
 }
