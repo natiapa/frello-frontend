48c7e3152bb42e9face8fa515404e11c8cbc76ea
Yana <yana102hanan@gmail.com>
2024-08-30 17:04:17 +0300

    fix grid layout in group list

--- src/cmps/GroupList.jsx
+++ src/cmps/GroupList.jsx
@@ -1,34 +1,52 @@
-import { useEffect, useState } from 'react'
-import { GroupPreview } from './GroupPreview'
-import { boardService } from '../services/board'
-import { useSelector } from 'react-redux'
-import { updateBoard } from '../store/actions/board.actions'
-import { AddingForm } from './AddingForm'
+import { useEffect, useState } from "react";
+import { GroupPreview } from "./GroupPreview";
+import { boardService } from "../services/board";
+import { useSelector } from "react-redux";
+import { updateBoard } from "../store/actions/board.actions";
+import { AddingForm } from "./AddingForm";
 
 export function GroupList({ groups }) {
-    const [isNewGroup, setIsNewGroup] = useState(false)
-    const [newGroup, setNewGroup] = useState(boardService.getEmptyGroup())
-    const currBoard = useSelector(state => state.boardModule.board)
+  const [isNewGroup, setIsNewGroup] = useState(false);
+  const [newGroup, setNewGroup] = useState(boardService.getEmptyGroup());
+  const currBoard = useSelector((state) => state.boardModule.board);
 
-    useEffect(() => {
-        console.log('render!')
-    }, [groups?.length])
+  useEffect(() => {
+    console.log("render!");
+  }, [groups?.length]);
 
-    function onAddGroup() {
-        setIsNewGroup(true)
-        setNewGroup(boardService.getEmptyGroup())
-    }
+  function onAddGroup() {
+    setIsNewGroup(true);
+    setNewGroup(boardService.getEmptyGroup());
+  }
 
-    if (!groups) return <div>Loading...</div>
-    return (
-        <div className="group-list">
-            {groups.map(group => (
-                <GroupPreview group={group} key={group.id} />
-            ))}
-            {isNewGroup && <AddingForm setIsNew={setIsNewGroup} setNew={setNewGroup} currBoard={currBoard} updateBoard={updateBoard} updated={newGroup} groupId={null} tasks={null} />}
-            <button className="add-group-btn" onClick={onAddGroup} hidden={isNewGroup}>
-                <span className="header-btn">+ Add another list</span>
-            </button>
-        </div>
-    )
+  if (!groups) return <div>Loading...</div>;
+  return (
+    <div
+      className="group-list"
+      style={{ gridTemplateColumns: `repeat(${groups.length + 1}, 284px)` }}
+    >
+      {groups.map((group) => (
+        <GroupPreview group={group} key={group.id} />
+      ))}
+      {isNewGroup && (
+        <AddingForm
+          setIsNew={setIsNewGroup}
+          setNew={setNewGroup}
+          currBoard={currBoard}
+          updateBoard={updateBoard}
+          updated={newGroup}
+          groupId={null}
+          tasks={null}
+        />
+      )}
+      <button
+        className="add-group-btn"
+        onClick={onAddGroup}
+        hidden={isNewGroup}
+        style={{ gridColumn: `${groups.length + 1}` }}
+      >
+        <span className="header-btn">+ Add another list</span>
+      </button>
+    </div>
+  );
 }
