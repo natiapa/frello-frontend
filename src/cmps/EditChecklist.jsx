17800f2a7a2f58458c8d4694ffc5e5c235b55c21
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2024-09-01 16:21:34 +0300

    Small fixes in Task

--- src/cmps/EditChecklist.jsx
+++ src/cmps/EditChecklist.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { boardService } from '../services/board'
 import { updateBoard } from '../store/actions/board.actions'
@@ -6,23 +6,33 @@ import { updateBoard } from '../store/actions/board.actions'
 export function EditChecklist({ groupId, taskId, task, setIsPopoverOpen, handlePopoverClick }) {
     const board = useSelector(state => state.boardModule.board)
     const [title, setTitle] = useState('checklist')
-    
+    const checklists = board.groups.find(group => group.id === groupId).tasks.find(task => task.id === taskId).checklists
+
+    useEffect(() => {
+        console.log('checklists:', checklists)
+    }, [checklists?.length])
+
     function handleChange(ev) {
         // ev.preventDefault()
         setTitle(ev.target.value)
     }
 
-    function onAddChecklist() {
-        boardService.updateBoard(board, groupId, taskId, {
-            key: 'checklists',
-            value: [...(task.checklists || ''), { ...boardService.getEmptyChecklist(), title: title }],
-        })
-        updateBoard(board)
+    async function onAddChecklist() {
+        checklists.push({ ...boardService.getEmptyChecklist(), title: title })
+        const updatedBoard = { ...board }
+        console.log('updatedBoard:', updatedBoard)
+        // const updatedBoard = boardService.updateBoard(board, groupId, taskId, {
+        //     key: 'checklists',
+        //     value: [...(task.checklists || ''), { ...boardService.getEmptyChecklist(), title: title }],
+        // })
+        updateBoard(updatedBoard) // מעדכן את הסטור של Redux עם ה-board המעודכן
         setIsPopoverOpen(false)
     }
     return (
         <div className="edit-checklist" onClick={handlePopoverClick}>
-            <h2>Checklist</h2>
+            <h2>
+                Checklist <span>{checklists?.length}</span>
+            </h2>
             <label>
                 <span>title</span>
                 <input type="text" value={title} onChange={handleChange} />
--- src/cmps/TaskDetailsActions.jsx
+++ src/cmps/TaskDetailsActions.jsx
@@ -1,170 +1,127 @@
-import { Popover, Typography } from "@mui/material";
-import { useState } from "react";
-import { CgCreditCard } from "react-icons/cg";
-import { IoMdCheckboxOutline } from "react-icons/io";
-import { TiTag } from "react-icons/ti";
-import { Link, useParams } from "react-router-dom";
-import { EditLables } from "./EditLabels";
-import { EditChecklist } from "./EditChecklist";
-import { SiDatefns } from "react-icons/si";
-import { LuClock5 } from "react-icons/lu";
-import { DueDatePicker } from "./DueDatePicker";
+import { Popover, Typography } from '@mui/material'
+import { useState } from 'react'
+import { CgCreditCard } from 'react-icons/cg'
+import { IoMdCheckboxOutline } from 'react-icons/io'
+import { TiTag } from 'react-icons/ti'
+import { Link, useParams } from 'react-router-dom'
+import { EditLables } from './EditLabels'
+import { EditChecklist } from './EditChecklist'
+import { SiDatefns } from 'react-icons/si'
+import { LuClock5 } from 'react-icons/lu'
+import { DueDatePicker } from './DueDatePicker'
+import { useSelector } from 'react-redux'
 
-export function TaskDetailsActions({
-  boardId,
-  groupId,
-  taskId,
-  task,
-  taskPrevActionsModalData,
-  setIsTaskPrevModalOpen,
-  selectedLabels,
-  setSelectedLabels,
-  onUpdated,
-}) {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-  const [modalOpenByName, setModalOpenByName] = useState(null);
+export function TaskDetailsActions({ boardId, groupId, taskId, task, taskPrevActionsModalData, setIsTaskPrevModalOpen, selectedLabels, setSelectedLabels, onUpdated }) {
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+    const [modalOpenByName, setModalOpenByName] = useState(null)
 
-  const { taskId: taskParams } = useParams();
+    const { taskId: taskParams } = useParams()
 
-  function handleClick(ev) {
-    const currDataName = ev.currentTarget.getAttribute("data-name");
-    setIsPopoverOpen((isOpen) => !isOpen);
-    setAnchorEl(ev.currentTarget);
-    setModalOpenByName(currDataName);
-  }
+    function handleClick(ev) {
+        const currDataName = ev.currentTarget.getAttribute('data-name')
+        setIsPopoverOpen(isOpen => !isOpen)
+        setAnchorEl(ev.currentTarget)
+        setModalOpenByName(currDataName)
+    }
 
-  function handlePopoverClick(ev) {
-    ev.stopPropagation();
-  }
+    function handlePopoverClick(ev) {
+        ev.stopPropagation()
+    }
 
-  return (
-    <section className="actions" style={{ ...taskPrevActionsModalData }}>
-      <button
-        data-name="checklists"
-        className="checklist action-btn"
-        aria-describedby="1"
-        onClick={handleClick}
-      >
-        <span className="icon">
-          <IoMdCheckboxOutline />
-        </span>
-        <p>Checklist </p>
+    return (
+        <section className="actions" style={{ ...taskPrevActionsModalData }}>
+            <button data-name="checklists" className="checklist action-btn" aria-describedby="1" onClick={handleClick}>
+                <span className="icon">
+                    <IoMdCheckboxOutline />
+                </span>
+                <p>Checklist</p>
 
-        {modalOpenByName === "checklists" && (
-          <Popover
-            id={anchorEl}
-            open={isPopoverOpen}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            disablePortal
-            // disableEnforceFocus
-            // disableAutoFocus
-          >
-            <Typography sx={{ p: 2 }} onClick={handlePopoverClick}></Typography>
+                {modalOpenByName === 'checklists' && (
+                    <Popover
+                        id={anchorEl}
+                        open={isPopoverOpen}
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        disablePortal
+                        // disableEnforceFocus
+                        // disableAutoFocus
+                    >
+                        <Typography sx={{ p: 2 }} onClick={handlePopoverClick}></Typography>
 
-            <EditChecklist
-              groupId={groupId}
-              taskId={taskId}
-              task={task}
-              setIsPopoverOpen={setIsPopoverOpen}
-              handlePopoverClick={handlePopoverClick}
-            />
-          </Popover>
-        )}
-      </button>
+                        <EditChecklist groupId={groupId} taskId={taskId} task={task} setIsPopoverOpen={setIsPopoverOpen} handlePopoverClick={handlePopoverClick} />
+                    </Popover>
+                )}
+            </button>
 
-      <button
-        data-name="due-date"
-        className="due-date action-btn"
-        aria-describedby="5"
-        onClick={handleClick}
-      >
-        <span className="icon">
-          <LuClock5 />
-        </span>
-        <p> Dates </p>
+            <button data-name="due-date" className="due-date action-btn" aria-describedby="5" onClick={handleClick}>
+                <span className="icon">
+                    <LuClock5 />
+                </span>
+                <p> Dates </p>
 
-        {modalOpenByName === "due-date" && (
-          <Popover
-            id={anchorEl}
-            open={isPopoverOpen}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            disablePortal
-            // disableEnforceFocus
-            // disableAutoFocus
-            PaperProps={{
-              sx: {
-                width: "400px",
-                height: "600px",
-                padding: "20px",
-              },
-            }}
-          >
-            <Typography sx={{ p: 2 }} onClick={handlePopoverClick}>
-              <DueDatePicker
-                onUpdated={onUpdated}
-              />
-            </Typography>
-          </Popover>
-        )}
-      </button>
+                {modalOpenByName === 'due-date' && (
+                    <Popover
+                        id={anchorEl}
+                        open={isPopoverOpen}
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        disablePortal
+                        // disableEnforceFocus
+                        // disableAutoFocus
+                        PaperProps={{
+                            sx: {
+                                width: '400px',
+                                height: '600px',
+                                padding: '20px',
+                            },
+                        }}>
+                        <Typography sx={{ p: 2 }} onClick={handlePopoverClick}>
+                            <DueDatePicker onUpdated={onUpdated} />
+                        </Typography>
+                    </Popover>
+                )}
+            </button>
 
-      {!taskParams && (
-        <Link
-          to={`/board/${boardId}/${groupId}/${taskId}`}
-          onClick={() => setIsTaskPrevModalOpen(false)}
-        >
-          <button className="open-card action-btn">
-            <span>
-              <CgCreditCard />
-            </span>
-            <p>open card</p>
-          </button>
-        </Link>
-      )}
+            {!taskParams && (
+                <Link to={`/board/${boardId}/${groupId}/${taskId}`} onClick={() => setIsTaskPrevModalOpen(false)}>
+                    <button className="open-card action-btn">
+                        <span>
+                            <CgCreditCard />
+                        </span>
+                        <p>open card</p>
+                    </button>
+                </Link>
+            )}
 
-      <button
-        aria-describedby="2"
-        data-name="labels"
-        className="edit-labels action-btn"
-        onClick={handleClick}
-      >
-        <span>
-          <TiTag />
-        </span>
-        <p>Edit Labels</p>
+            <button aria-describedby="2" data-name="labels" className="edit-labels action-btn" onClick={handleClick}>
+                <span>
+                    <TiTag />
+                </span>
+                <p>Edit Labels</p>
 
-        {modalOpenByName === "labels" && (
-          <Popover
-            id={anchorEl}
-            open={isPopoverOpen}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-            disablePortal
-          >
-            <EditLables
-              groupId={groupId}
-              task={task}
-              handlePopoverClick={handlePopoverClick}
-              selectedLabels={selectedLabels}
-              setSelectedLabels={setSelectedLabels}
-            />
-          </Popover>
-        )}
-      </button>
-    </section>
-  );
+                {modalOpenByName === 'labels' && (
+                    <Popover
+                        id={anchorEl}
+                        open={isPopoverOpen}
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        disablePortal>
+                        <EditLables groupId={groupId} task={task} handlePopoverClick={handlePopoverClick} selectedLabels={selectedLabels} setSelectedLabels={setSelectedLabels} />
+                    </Popover>
+                )}
+            </button>
+        </section>
+    )
 }
 
 {
