981d48a97e891a250199de761b22eaa365df8d83
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2024-09-01 14:11:19 +0300

    More styling for adding a group and adding a task

--- src/cmps/TaskList.jsx
+++ src/cmps/TaskList.jsx
@@ -1,65 +1,67 @@
-import { useEffect, useState } from "react";
-import { TaskPreview } from "./TaskPreview";
-import { useSelector } from "react-redux";
-import { updateBoard } from "../store/actions/board.actions";
-import { AddingForm } from "./AddingForm";
-import { Link } from "react-router-dom";
-import { Droppable } from "react-beautiful-dnd";
+import { useEffect, useRef, useState } from 'react'
+import { TaskPreview } from './TaskPreview'
+import { useSelector } from 'react-redux'
+import { updateBoard } from '../store/actions/board.actions'
+import { AddingForm } from './AddingForm'
+import { Link } from 'react-router-dom'
+import { Droppable } from 'react-beautiful-dnd'
 
 export function TaskList({ group }) {
-  const { tasks } = group;
+    const { tasks } = group
 
-  const [isNewTask, setIsNewTask] = useState(false);
-  const [newTask, setNewTask] = useState(boardService.getEmptyTask());
-  const board = useSelector((state) => state.boardModule.board);
+    const [isNewTask, setIsNewTask] = useState(false)
+    const [newTask, setNewTask] = useState(boardService.getEmptyTask())
+    const board = useSelector(state => state.boardModule.board)
+    const taskListRef = useRef(null)
+    const addingFormRef = useRef(null)
 
-  useEffect(() => {
-    console.log("render!");
-  }, [group?.length]);
+    useEffect(() => {
+        if (isNewTask && addingFormRef.current && taskListRef.current) {
+            taskListRef.current.scrollTop = addingFormRef.current.offsetTop
+            document.querySelector('textarea').placeholder = 'Enter a name for this card...'
+        }
+    }, [isNewTask])
+    useEffect(() => {
+        console.log('render!')
+    }, [group?.length])
 
-  function onAddTask() {
-    setIsNewTask(true);
-    setNewTask(boardService.getEmptyTask());
-  }
+    function onAddTask() {
+        setIsNewTask(true)
+        setNewTask(boardService.getEmptyTask())
+    }
 
-  return (
-    <>
-      <Droppable droppableId={group.id} type="task">
-        {(provided) => (
-          <ul
-            className="task-list"
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            {tasks.map((task, tIndex) => (
-              <TaskPreview
-                groupId={group.id}
-                task={task}
-                key={task.id}
-                tIndex={tIndex}
-              />
-            ))}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
-      <div className="footer-group">
-        {isNewTask && (
-          <AddingForm
-            setIsNew={setIsNewTask}
-            setNew={setNewTask}
-            currBoard={board}
-            updateBoard={updateBoard}
-            updated={newTask}
-            groupId={group.id}
-            tasks={group.tasks}
-          />
-        )}
-
-        <button className="add-task-btn" onClick={onAddTask} hidden={isNewTask}>
-          <span className="header-btn">+ Add a card</span>
-        </button>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Droppable droppableId={group.id} type="task">
+                {provided => (
+                    <ul
+                        className="task-list"
+                        ref={element => {
+                            provided.innerRef(element)
+                            taskListRef.current = element
+                        }}
+                        {...provided.droppableProps}>
+                        {tasks.map((task, tIndex) => (
+                            <TaskPreview groupId={group.id} task={task} key={task.id} tIndex={tIndex} />
+                        ))}
+                        {provided.placeholder}
+                        {isNewTask && (
+                            <div ref={addingFormRef}>
+                                <AddingForm setIsNew={setIsNewTask} setNew={setNewTask} currBoard={board} updateBoard={updateBoard} updated={newTask} groupId={group.id} tasks={group.tasks} />
+                            </div>
+                        )}
+                    </ul>
+                )}
+            </Droppable>
+            <div className="footer-group">
+                {
+                    <button className="add-task-btn" onClick={onAddTask}>
+                        <span className="header-btn" style={isNewTask ? { opacity: '0' } : { opacity: '1' }}>
+                            + Add a card
+                        </span>
+                    </button>
+                }
+            </div>
+        </>
+    )
 }
