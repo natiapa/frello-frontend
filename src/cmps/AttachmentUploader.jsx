bd329805a9336107f030e1771fceb0d354d5ccc8
natiapa <natiapanosian@gmail.com>
2024-09-02 13:11:00 +0300

    Add library

--- src/cmps/AttachmentUploader.jsx
+++ src/cmps/AttachmentUploader.jsx
@@ -1,12 +1,22 @@
+import { useDropzone } from "react-dropzone";
+
 export function AttachmentUploader({
   onUpdated,
   setIsPopoverOpen,
   handlePopoverClick,
 }) {
-  return (
-    <div>
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/*,application/pdf',
+        onDrop: acceptedFiles => {
+          console.log(acceptedFiles);
+        }
+      });
+    
+      return (
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+      );
+    }
 
-      <h1> Attach</h1>
-    </div>
-  );
-}
--- src/cmps/EditChecklist.jsx
+++ src/cmps/EditChecklist.jsx
@@ -1,42 +1,54 @@
-import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { boardService } from '../services/board'
-import { updateBoard } from '../store/actions/board.actions'
-import { useParams } from 'react-router'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { boardService } from "../services/board";
+import { updateBoard } from "../store/actions/board.actions";
+import { useParams } from "react-router";
 
-export function EditChecklist({ groupId, taskId, task, onUpdated, setIsPopoverOpen, handlePopoverClick, setNewCheckLists }) {
-    const board = useSelector(state => state.boardModule.board)
-    const [title, setTitle] = useState('checklist')
-    const checklists = board.groups.find(group => group.id === groupId).tasks.find(task => task.id === taskId).checklists || []
+export function EditChecklist({
+  groupId,
+  taskId,
+  task,
+  onUpdated,
+  setIsPopoverOpen,
+  handlePopoverClick,
+  setNewCheckLists,
+}) {
+  const board = useSelector((state) => state.boardModule.board);
+  const [title, setTitle] = useState("checklist");
+  const checklists =
+    board.groups
+      .find((group) => group.id === groupId)
+      .tasks.find((task) => task.id === taskId).checklists || [];
 
-    useEffect(() => {
-        console.log('checklists:', checklists)
-        setNewCheckLists(checklists)
-    }, [checklists?.length])
+  useEffect(() => {
+    console.log("checklists:", checklists);
+    setNewCheckLists(checklists);
+  }, [checklists?.length]);
 
-    function handleChange(ev) {
-        setTitle(ev.target.value)
-    }
+  function handleChange(ev) {
+    setTitle(ev.target.value);
+  }
 
-   async function onAddChecklist() {
-        boardService.updateBoard(board, groupId, taskId, {
-            key: 'checklists',
-            value: [...(task.checklists || ''), { ...boardService.getEmptyChecklist(), title: title }],
-        })
-        await updateBoard(board)
-        setIsPopoverOpen(false)
-    }
-    
-    return (
-        <div className="edit-checklist" onClick={handlePopoverClick}>
-            <h2>Checklist span</h2>
-            <label>
-                <span>title</span>
-                <input type="text" value={title} onChange={handleChange} />
-            </label>
-            <div className="edit-checklist-btn" onClick={onAddChecklist}>
-                Add
-            </div>
-        </div>
-    )
+  async function onAddChecklist() {
+    checklists.push({ ...boardService.getEmptyChecklist(), title });
+    console.log(checklists);
+    setNewCheckLists(checklists);
+
+    // console.log(checklists);
+    onUpdated("checklists", checklists);
+    setIsPopoverOpen(false);
+  }
+
+  return (
+    <div className="edit-checklist" onClick={handlePopoverClick}>
+      <h2>Checklist span</h2>
+      <label>
+        <span>title</span>
+        <input type="text" value={title} onChange={handleChange} />
+      </label>
+      <div className="edit-checklist-btn" onClick={onAddChecklist}>
+        Add
+      </div>
+    </div>
+  );
 }
