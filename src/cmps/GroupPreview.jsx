141f27ecfcaea2685ac13791b5a8e5be0334f1f1
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2024-09-04 20:44:47 +0300

    Hi dots icon

--- src/cmps/GroupPreview.jsx
+++ src/cmps/GroupPreview.jsx
@@ -1,84 +1,121 @@
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router";
-import { boardService } from "../services/board";
-import { updateBoard } from "../store/actions/board.actions";
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import { boardService } from '../services/board'
+import { updateBoard } from '../store/actions/board.actions'
 
-import { TaskList } from "./TaskList";
-import { useEffect, useState } from "react";
-import { Edit } from "./Edit";
-import { Draggable } from "react-beautiful-dnd";
+import { TaskList } from './TaskList'
+import { useEffect, useState } from 'react'
+import { Edit } from './Edit'
+import { Draggable } from 'react-beautiful-dnd'
+import { HiDotsHorizontal } from 'react-icons/hi'
+import { Popover } from '@mui/material'
 
 export function GroupPreview({ group, gIndex, allowDrop, drop }) {
-  const board = useSelector((storeState) => storeState.boardModule.board);
+    const board = useSelector(storeState => storeState.boardModule.board)
 
-  const [currElToEdit, setCurrElToEdit] = useState("");
+    const [currElToEdit, setCurrElToEdit] = useState('')
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [isHiDotsOpen, setIsHiDotsOpen] = useState(false)
 
-  const navigate = useNavigate();
+    const navigate = useNavigate()
 
-  async function deleteGroup(ev) {
-    ev.preventDefault();
+    async function deleteGroup(ev) {
+        ev.preventDefault()
 
-    try {
-      const newBoard = await boardService.updateBoard(board, group.id, null, {
-        key: "deleteGroup",
-        value: null,
-      });
-      await updateBoard(newBoard);
-    } catch (error) {
-      console.error("Failed to update the board:", error);
+        try {
+            const newBoard = await boardService.updateBoard(
+                board,
+                group.id,
+                null,
+                {
+                    key: 'deleteGroup',
+                    value: null,
+                }
+            )
+            await updateBoard(newBoard)
+        } catch (error) {
+            console.error('Failed to update the board:', error)
+        }
+
+        navigate(`/board/${board._id}`)
+    }
+    function onEdit({ target }) {
+        const dataName = target.getAttribute('data-name')
+        setCurrElToEdit(dataName)
     }
 
-    navigate(`/board/${board._id}`);
-  }
-  function onEdit({ target }) {
-    const dataName = target.getAttribute("data-name");
-    setCurrElToEdit(dataName);
-  }
-
-  async function onUpdated(name, value) {
-    try {
-      const updatedBoard = boardService.updateBoard(board, group.id, null, {
-        key: name,
-        value: value,
-      });
-      await updateBoard(updatedBoard);
-    } catch (error) {
-      console.error("Failed to update the board:", error);
+    function onOpen(ev) {
+        setAnchorEl(ev.currentTarget)
+        setIsHiDotsOpen(isHiDotsOpen => !isHiDotsOpen)
     }
-  }
 
-  return (
-    <Draggable key={group.id} draggableId={group.id} index={gIndex}>
-      {(provided, snapshot) => (
-        <li
-          className="group-preview"
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          style={{
-            ...provided.draggableProps.style,
-            opacity: snapshot.isDragging ? "0.5" : "1",
-          }}
-        >
-          <div className="header-and-actions">
-            {currElToEdit !== "title" && (
-              <h3 data-name="title" onClick={onEdit}>
-                {group?.title}
-              </h3>
+    async function onUpdated(name, value) {
+        try {
+            const updatedBoard = boardService.updateBoard(
+                board,
+                group.id,
+                null,
+                {
+                    key: name,
+                    value: value,
+                }
+            )
+            await updateBoard(updatedBoard)
+        } catch (error) {
+            console.error('Failed to update the board:', error)
+        }
+    }
+
+    return (
+        <Draggable key={group.id} draggableId={group.id} index={gIndex}>
+            {(provided, snapshot) => (
+                <li
+                    className="group-preview"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={{
+                        ...provided.draggableProps.style,
+                        opacity: snapshot.isDragging ? '0.5' : '1',
+                    }}>
+                    <div className="header-and-actions">
+                        {currElToEdit !== 'title' && (
+                            <h3 data-name="title" onClick={onEdit}>
+                                {group?.title}
+                            </h3>
+                        )}
+                        {currElToEdit === 'title' && (
+                            <Edit
+                                task={group}
+                                currElToEdit={currElToEdit}
+                                onUpdated={onUpdated}
+                                setCurrElToEdit={setCurrElToEdit}
+                            />
+                        )}
+                        <div className="hi-dots" onClick={onOpen}>
+                            <HiDotsHorizontal />
+
+                            {isHiDotsOpen && (
+                                <Popover
+                                    open={isHiDotsOpen}
+                                    id={anchorEl}
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'right',
+                                    }}>
+                                    <div className="group-preview-actions">
+                                        <button onClick={deleteGroup}>
+                                            Delete
+                                        </button>
+                                    </div>
+                                </Popover>
+                            )}
+                        </div>
+                    </div>
+                    <TaskList group={group} allowDrop={allowDrop} drop={drop} />
+                </li>
             )}
-            {currElToEdit === "title" && (
-              <Edit
-                task={group}
-                currElToEdit={currElToEdit}
-                onUpdated={onUpdated}
-                setCurrElToEdit={setCurrElToEdit}
-              />
-            )}
-            <button onClick={deleteGroup}>Delete list</button>
-          </div>
-          <TaskList group={group} allowDrop={allowDrop} drop={drop} />
-        </li>
-      )}
-    </Draggable>
-  );
+        </Draggable>
+    )
 }
