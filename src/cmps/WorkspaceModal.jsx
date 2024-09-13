9359abb334286fc259a8603152804180c4ce5a08
Yana <yana102hanan@gmail.com>
2024-09-13 22:56:13 +0300

    delete lines

--- src/cmps/WorkspaceModal.jsx
+++ src/cmps/WorkspaceModal.jsx
@@ -5,16 +5,10 @@ export function WorkspaceModal({ setIsPopoverOpen, handlePopoverClick }) {
   const navigate = useNavigate();
   return (
     <div className="workspace-container" onClick={handlePopoverClick}>
-      {/* <header className="workspace-header">
-        <span className="close-btn" onClick={() => setIsPopoverOpen(false)}>
-          <SvgIcon iconName="close" />
-        </span>
-        <h3>Workspace</h3>
-      </header> */}
       <h2>Your Workspaces</h2>
       <a onClick={() => navigate("/board")}>
-        <div className="color">T</div>
-        <span>Trello Workspace</span>
+        <div className="color">F</div>
+        <span>Frello Workspace</span>
       </a>
     </div>
   );
